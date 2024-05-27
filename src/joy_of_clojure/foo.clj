71a6b8baaf1f9fc2a23c126d5331afcd9c0b6786
Saurav S Purushothaman <saurav.purushothaman@unifize.com>
2024-05-27 20:50:11 +0530

    Use lazy sequence to create fibinocci numbers

--- src/joy_of_clojure/foo.clj
+++ src/joy_of_clojure/foo.clj
@@ -13,4 +13,16 @@
        (take n)
        rest))
 
-(fibinocci 10)
+;; There is also another way to create fibinocci series in clojure.
+;; Using lazy sequences.
+
+;; NOTE: Whem making fixed size values, always user recur
+;; Otherwise
+
+
+(defn lazy-seq-fibinocci
+  "Genereates fibinocci series using lazy sequences"
+  ([] (concat [0 1] (lazy-seq-fibinocci 0 1)))
+  ([a b]
+   (lazy-seq
+    (cons (+ a b) (lazy-seq-fibinocci b (+ a b))))))
