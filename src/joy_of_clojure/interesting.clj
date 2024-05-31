62c1ac080a6572a5737fc39c448b9f9e5c96f2f9
Saurav S Purushothaman <saurav.purushothaman@unifize.com>
2024-06-01 00:55:55 +0530

    Add functions for evaluating math expressions with fixity rules
    - Introduced l->rfix function to evaluate expressions from left to right.
      - Supports binary, ternary, and quaternary operations.
    - Introduced r->lfix function to evaluate expressions from right to left.
      - Supports binary, ternary, and quaternary operations.
    - Added tests for l->rfix and r->lfix demonstrating correct behavior.
    - Defined order map to set precedence for operators (+, -, *, /).
    - Implemented infix3 function to evaluate infix expressions
      considering operator precedence.
      - infix3 chooses the correct evaluation strategy based on operator precedence.

---
+++ src/joy_of_clojure/interesting.clj
@@ -0,0 +1,35 @@
+(ns joy-of-clojure.interesting)
+;; this namespace contains interesting programs/functions
+
+(defn l->rfix
+  "Function that solves math equations,evaluated left to right"
+  ([a op b] (op a b))
+  ([a op1 b op2 c] (op2 c (op1 b a)))
+  ([a op1 b op2 c op3 d] (op3 d (op2 c (op1 b a)))))
+
+(defn r->lfix
+  "Function that solves math equations,evaluated left to right"
+  ([a op b] (op a b))
+  ([a op1 b op2 c] (op1 a (op2 b c)))
+  ([a op1 b op2 c op3 d] (op1 a (op2 b (op3 c d)))))
+
+(r->lfix 1 + 2)          ;; => 3
+(r->lfix 1 + 2 + 3)      ;; => 6
+(r->lfix 1 + 2 + 3 + 4)  ;; => 10
+
+(l->rfix 1 + 2)          ;; => 3
+(l->rfix 1 + 2 + 3)      ;; => 6
+(l->rfix 1 + 2 + 3 + 4)  ;; => 10
+
+;; Creating precedence for operands
+(def order {+ 0 - 0 * 1 / 1})
+
+(defn infix3 [a op1 b op2 c]
+  "Function that converts prefix notation to infix notation and
+  evaluates the mathematical expression"
+  (if (> (get order op1) (get order op2))
+    (l->rfix a op1 b op2 c)
+    (r->lfix a op1 b op2 c)))
+
+(infix3 10 * 2 + 3)  ;; => 23
+(infix3 10 + 2 * 3)  ;; => 16
