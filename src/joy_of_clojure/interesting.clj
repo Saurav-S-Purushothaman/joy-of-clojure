dbfb2586be1de89b007ebc4f75c83b6aa2ff6144
Saurav S Purushothaman <saurav.purushothaman@unifize.com>
2024-06-01 18:45:28 +0530

    Add source code of `+`

--- src/joy_of_clojure/interesting.clj
+++ src/joy_of_clojure/interesting.clj
@@ -33,3 +33,21 @@
 
 (infix3 10 * 2 + 3)  ;; => 23
 (infix3 10 + 2 * 3)  ;; => 16
+
+;; this is the source of the '+' operator
+;; you can get it by running this command in the repl
+(source +)
+;; how cool is that!!!!
+
+
+(defn +
+  "Returns the sum of nums. (+) returns 0. Does not auto-promote
+  longs, will throw on overflow. See also: +'"
+  {:inline (nary-inline 'add 'unchecked_add)
+   :inline-arities >1?
+   :added "1.2"}
+  ([] 0)
+  ([x] (cast Number x))
+  ([x y] (. clojure.lang.Numbers (add x y)))
+  ([x y & more]
+   (reduce1 + (+ x y) more)))
