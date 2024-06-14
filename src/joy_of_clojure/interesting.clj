bb84a14bcc6d4e486eebbc622b6354d450baa996
Saurav S Purushothaman <saurav.purushothaman@unifize.com>
2024-06-14 23:05:39 +0530

    Solve bug in source code of addition

--- src/joy_of_clojure/interesting.clj
+++ src/joy_of_clojure/interesting.clj
@@ -40,17 +40,14 @@
 ;; how cool is that!!!!
 
 
-(defn +
+(defn plus
   "Returns the sum of nums. (+) returns 0. Does not auto-promote
   longs, will throw on overflow. See also: +'"
-  {:inline (nary-inline 'add 'unchecked_add)
-   :inline-arities >1?
-   :added "1.2"}
   ([] 0)
   ([x] (cast Number x))
   ([x y] (. clojure.lang.Numbers (add x y)))
   ([x y & more]
-   (reduce1 + (+ x y) more)))
+   (reduce + (+ x y) more)))
 
 
 ;; easiest definition of apply
