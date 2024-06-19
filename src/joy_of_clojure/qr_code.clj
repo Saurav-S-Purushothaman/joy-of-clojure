74749377c1fc60bbb1840195e8efb0e61d41d8af
Saurav S Purushothaman <saurav.purushothaman@unifize.com>
2024-06-19 17:03:08 +0530

    Generate QR code

---
+++ src/joy_of_clojure/qr_code.clj
@@ -0,0 +1,14 @@
+(ns joy-of-clojure.qr-code
+  (:require [clj.qrgen :as qr]
+            [clojure.java.io :as io]))
+
+(qr/as-fi (qr/from "https://www.google.com")
+            "/User/sauravaspurushothamanQRCode.png")
+
+(def data (qr/as-bytes (qr/from "https://app.unifize.com/conversation/5E76KNKPD?templateId=469" :size [250 250] :image-type qr/PNG)))
+
+(defn save-image [byte-array filename]
+  (with-open [w (clojure.java.io/output-stream filename)]
+    (.write w byte-array)))
+
+(save-image data "image.png")
