b544924524bc12dc2a20e70739eede8d81f008a0
Saurav S Purushothaman <saurav.purushothaman@unifize.com>
2024-06-25 20:55:09 +0530

    Add function to find difference between dates

--- src/joy_of_clojure/small-syntax.clj
+++ src/joy_of_clojure/small-syntax.clj
@@ -1,4 +1,5 @@
 (ns joy-of-clojure.small-syntax
+  (:import [java.time LocalDate Period])
   (:gen-class))
 
 ;; This namespace contains detaials about several clojure functions
@@ -15,3 +16,19 @@
 
 (doseq [x [:a :b], y (range 5) :when (odd? y)]
   (prn [x y]))
+
+(defn date-difference
+  "Finds the difference in date"
+  [date-1 date-2]
+  (let [d1 (LocalDate/parse date-1)
+        d2 (LocalDate/parse date-2)
+        period (Period/between d2 d1)]
+    {
+     :year (.getYears period)
+     :months (.getMonths period)
+     :days (.getDays period)
+     }
+    ))
+
+
+(date-difference "2024-06-25" "2024-02-19")
